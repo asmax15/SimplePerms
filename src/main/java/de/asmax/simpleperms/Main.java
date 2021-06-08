877511e9e2688ea42794bec32e7866829e08ad45
asmax15 <max.aschmann@gmail.com>
2021-06-08 21:50:39 +0200

    Initial commit

---
+++ src/main/java/de/asmax/simpleperms/Main.java
@@ -0,0 +1,68 @@
+package de.asmax.simpleperms;
+
+import de.asmax.simpleperms.commands.AddPermissionCommand;
+import de.asmax.simpleperms.commands.AddTempPermission;
+import de.asmax.simpleperms.commands.RemovePermissionCommand;
+import de.asmax.simpleperms.permissions.PermissionManager;
+import de.asmax.simpleperms.utils.Config;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    private static Main instance;
+    private String prefix = "§8[§9SimplePerms§8] §7";
+    private String error = "§8[§4ERROR§8] §f";
+    private Config config;
+    private PermissionManager permissionManager;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+        config = new Config();
+    }
+
+    @Override
+    public void onEnable() {
+        CommandRegistration();
+        ListenerRegistration();
+
+        permissionManager = new PermissionManager();
+    }
+
+    @Override
+    public void onDisable() {
+        config.save();
+    }
+
+    private void CommandRegistration() {
+        getCommand("spadd").setExecutor(new AddPermissionCommand());
+        getCommand("spremove").setExecutor(new RemovePermissionCommand());
+        getCommand("sptemp").setExecutor(new AddTempPermission());
+    }
+
+    private void ListenerRegistration() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public Config getConfiguration() {
+        return config;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public PermissionManager getPermissionManager() {
+        return permissionManager;
+    }
+}
